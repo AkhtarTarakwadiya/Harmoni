cf472921d48bfa4422065503cbe39112b87a7269
sohelkhan19 <pathansohel2330@gmail.com>
2025-03-05 09:54:40 +0530

    User Follow - Unfollow API Completed

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "harmoni";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$conn){
+    die("Connection failed". mysqli_connect_error());
+}
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description, X-Requested-With, Authorization');
