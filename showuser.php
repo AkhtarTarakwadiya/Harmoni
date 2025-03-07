e585812581ced0ba01fa9c60a3e2718cb3306b7a
Akhtar-47 <akhtar47tarakwadiya@gmail.com>
2025-03-07 14:31:00 +0530

    Show Users Page Updated

--- showuser.php
+++ showuser.php
@@ -1,6 +1,5 @@
 <?php
-include 'config/db.php';
-// include './database/db.php';
+include './database/db.php';
 
 // Fetch user data
 $sql = "SELECT 
@@ -9,7 +8,7 @@ $sql = "SELECT
             user_email, 
             gender, 
             user_profile_photo, 
-            user_bio,  -- Missing comma was here
+            user_bio,
             (SELECT COUNT(*) FROM posts WHERE user_id = u.user_id AND post_status = 1) AS total_posts,
             (SELECT COUNT(*) FROM follow_master WHERE following_id = u.user_id) AS total_followers,
             (SELECT COUNT(*) FROM follow_master WHERE follower_id = u.user_id) AS total_following 
@@ -17,13 +16,11 @@ $sql = "SELECT
         WHERE user_status = 1 AND user_isblock = 1";
 
 $result = mysqli_query($conn, $sql);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -32,93 +29,61 @@ $result = mysqli_query($conn, $sql);
 
     <title>Harmoni Admin - Show Users</title>
 
-    <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
     <link rel="stylesheet" href="css/style.css">
-
-
 </head>
 
 <body id="page-top">
-
-    <!-- Page Wrapper -->
     <div id="wrapper">
-
-        <!-- Sidebar -->
         <?php include 'common/side.php'; ?>
-        <!-- End of Sidebar -->
 
-
-        <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
             <div id="content">
-
-                <!-- Topbar -->
                 <?php include 'common/nav.php'; ?>
-                <!-- End of Topbar -->
 
-                <!-- Begin Page Content -->
                 <div class="container-fluid">
-
-                    <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Show Users</h1>
-                        <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> -->
                     </div>
 
                     <div class="row">
-            <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-                <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="profile-card">
-                        <img src="Harmoni/<?php echo htmlspecialchars($row['user_profile_photo']); ?>" class="profile-pic" alt="Profile Pic">
-                        <div class="username">@<?php echo htmlspecialchars($row['user_name']); ?></div>
-                        <div class="fullname"><?php echo htmlspecialchars($row['user_full_name']); ?></div>
-                        <div class="gender">Gender: <?php echo htmlspecialchars($row['gender']); ?></div>
-                        <div class="email">Email: <?php echo htmlspecialchars($row['user_email']); ?></div>
-                        <div class="bio"><?php echo htmlspecialchars($row['user_bio'] ?: 'No bio available'); ?></div>
-                        <div class="stats">
-                            <div><span><?php echo $row['total_posts']; ?></span> Posts</div>
-                            <div><span><?php echo $row['total_followers']; ?></span> Followers</div>
-                            <div><span><?php echo $row['total_following']; ?></span> Following</div>
-                        </div>
+                        <?php while ($row = mysqli_fetch_assoc($result)) { 
+                            // Set profile image URL
+                            $profileImage = !empty($row['user_profile_photo']) ? 
+                                "http://192.168.4.220/Harmoni" . $row['user_profile_photo'] : 
+                                "http://192.168.4.220/Harmoni/uploads/default_profile.png";
+                        ?>
+                            <div class="col-lg-4 col-md-6 mb-4">
+                                <div class="profile-card">
+                                    <img src="<?php echo htmlspecialchars($profileImage); ?>" class="profile-pic" alt="Profile Pic">
+                                    <div class="username">@<?php echo htmlspecialchars($row['user_name']); ?></div>
+                                    <div class="fullname"><?php echo htmlspecialchars($row['user_full_name']); ?></div>
+                                    <div class="gender">Gender: <?php echo htmlspecialchars($row['gender']); ?></div>
+                                    <div class="email">Email: <?php echo htmlspecialchars($row['user_email']); ?></div>
+                                    <div class="bio"><?php echo htmlspecialchars($row['user_bio'] ?: 'No bio available'); ?></div>
+                                    <div class="stats">
+                                        <div><span><?php echo $row['total_posts']; ?></span> Posts</div>
+                                        <div><span><?php echo $row['total_followers']; ?></span> Followers</div>
+                                        <div><span><?php echo $row['total_following']; ?></span> Following</div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php } ?>
                     </div>
                 </div>
-            <?php } ?>
-        </div>
-                </div>
-                <!-- /.container-fluid -->
-
             </div>
-            <!-- End of Main Content -->
 
-            <!-- Footer -->
             <?php include 'common/footer.php'; ?>
-            <!-- End of Footer -->
-
         </div>
-        <!-- End of Content Wrapper -->
-
     </div>
-    <!-- End of Page Wrapper -->
 
-    <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -136,25 +101,13 @@ $result = mysqli_query($conn, $sql);
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
     <script src="vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
-
-
-
 </body>
 
 </html>
