fe3a751f2eabb771cd94c46c926321bd0a8a6724
sohelkhan19 <pathansohel2330@gmail.com>
2025-03-10 11:19:31 +0530

    Displayed user posts dynamically

--- showpost.php
+++ showpost.php
@@ -1,3 +1,28 @@
+<?php
+include 'database/db.php';
+
+// Query to fetch posts, media, likes, and comments count
+$fetchPostsQuery = "
+    SELECT 
+        p.post_id, 
+        p.user_id, 
+        u.user_name, 
+        p.post_content, 
+        p.created_at, 
+        GROUP_CONCAT(DISTINCT pm.media) AS media_files,
+        COUNT(DISTINCT pl.id) AS like_count,
+        COUNT(DISTINCT pc.comment_id) AS comment_count
+    FROM posts p
+    LEFT JOIN user_master u ON p.user_id = u.user_id
+    LEFT JOIN posts_media_master pm ON p.post_id = pm.post_id
+    LEFT JOIN likes_master pl ON p.post_id = pl.post_id
+    LEFT JOIN comments_master pc ON p.post_id = pc.post_id
+    WHERE p.post_status = 1
+    GROUP BY p.post_id 
+    ORDER BY p.created_at DESC";
+
+$result = mysqli_query($conn, $fetchPostsQuery);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -53,83 +78,66 @@
                     </div>
 
                     <div class="row">
+                        <?php while ($row = mysqli_fetch_assoc($result)) {
+                            $post_id = (int)$row['post_id'];
 
-                        <div class="col-lg-4 col-md-6 mb-4">
-                            <div class="post-card">
-                                <!-- Bootstrap Carousel for Images/Videos (No Auto-Slide) -->
-                                <div id="post1Carousel" class="carousel slide post-carousel">
-                                    <div class="carousel-inner">
-                                        <div class="carousel-item active">
-                                            <img src="img/car1.jpg" alt="Post Image">
+                            // Media file formatting - Full URL Path
+                            $mediaFiles = !empty($row['media_files'])
+                                ? array_map(fn($file) => "http://192.168.4.220/Harmoni/uploads/posts/" . $file, explode(',', $row['media_files']))
+                                : [];
+                        ?>
+                            <div class="col-lg-4 col-md-6 mb-4">
+                                <div class="post-card">
+                                    <div id="carousel_<?php echo $post_id; ?>" class="carousel slide post-carousel">
+                                        <div class="carousel-inner">
+                                            <?php if (!empty($mediaFiles)) {
+                                                foreach ($mediaFiles as $index => $media) {
+                                                    $fileExtension = pathinfo($media, PATHINFO_EXTENSION);
+                                            ?>
+                                                    <div class="carousel-item <?php echo $index == 0 ? 'active' : ''; ?>">
+                                                        <?php if (in_array(strtolower($fileExtension), ['jpg', 'jpeg', 'png', 'gif'])) { ?>
+                                                            <img src="<?php echo htmlspecialchars($media); ?>" alt="Post Image">
+                                                        <?php } elseif (in_array(strtolower($fileExtension), ['mp4', 'webm', 'ogg'])) { ?>
+                                                            <video controls>
+                                                                <source src="<?php echo htmlspecialchars($media); ?>" type="video/<?php echo $fileExtension; ?>">
+                                                                Your browser does not support the video tag.
+                                                            </video>
+                                                        <?php } ?>
+                                                    </div>
+                                                <?php }
+                                            } else { ?>
+                                                <div class="carousel-item active">
+                                                    <img src="http://192.168.4.220/Harmoni/uploads/default.jpg" alt="No Image Available">
+                                                </div>
+                                            <?php } ?>
                                         </div>
-                                        <div class="carousel-item">
-                                            <img src="img/car2.jpg" alt="Post Image">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <video controls>
-                                                <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
-                                                Your browser does not support the video tag.
-                                            </video>
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#carousel_<?php echo $post_id; ?>" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon"></span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#carousel_<?php echo $post_id; ?>" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon"></span>
+                                        </button>
+                                    </div>
+
+                                    <div class="post-meta">
+                                        <div class="username">@<?php echo htmlspecialchars($row['user_name']); ?></div>
+                                        <div class="post-stats">
+                                            <span class="likes-count" data-post="<?php echo $post_id; ?>" onclick="openModal('likes', <?php echo $post_id; ?>)">
+                                                <i class="fas fa-heart"></i> <?php echo $row['like_count']; ?>
+                                            </span>
+                                            <span class="comments-count" data-post="<?php echo $post_id; ?>" onclick="openModal('comments', <?php echo $post_id; ?>)">
+                                                <i class="fas fa-comment"></i> <?php echo $row['comment_count']; ?>
+                                            </span>
                                         </div>
                                     </div>
-                                    <button class="carousel-control-prev" type="button" data-bs-target="#post1Carousel" data-bs-slide="prev">
-                                        <span class="carousel-control-prev-icon"></span>
-                                    </button>
-                                    <button class="carousel-control-next" type="button" data-bs-target="#post1Carousel" data-bs-slide="next">
-                                        <span class="carousel-control-next-icon"></span>
-                                    </button>
-                                </div>
 
-                                <!-- Post Content -->
-                                <div class="post-meta">
-                                    <div class="username">@jone_doe</div>
-                                    <div class="post-stats">
-                                        <span class="likes-count"><i class="fas fa-heart"></i> 20</span>
-                                        <span class="comments-count"><i class="fas fa-comment"></i> 15</span>
+                                    <div class="description" id="desc_<?php echo $post_id; ?>">
+                                        <?php echo htmlspecialchars(substr($row['post_content'], 0, 100)) . '...'; ?>
                                     </div>
+                                    <span class="see-more" onclick="toggleDescription('desc_<?php echo $post_id; ?>', this)">See More</span>
                                 </div>
-                                <div class="description" id="desc1">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam... Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, error recusandae eaque, cumque accusamus temporibus eius atque doloribus fugit quibusdam laborum iusto, incidunt aliquam exercitationem sit voluptatem. Itaque, dolor quasi?
-                                </div>
-                                <span class="see-more" onclick="toggleDescription('desc1', this)">See More</span>
                             </div>
-                        </div>
-
-                        <!-- Post 2 -->
-                        <div class="col-lg-4 col-md-6 mb-4">
-                            <div class="post-card">
-                                <div id="post2Carousel" class="carousel slide post-carousel">
-                                    <div class="carousel-inner">
-                                        <div class="carousel-item active">
-                                            <img src="img/car3.jpg" alt="Post Image">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img src="img/car1.jpg" alt="Post Image">
-                                        </div>
-                                    </div>
-                                    <button class="carousel-control-prev" type="button" data-bs-target="#post2Carousel" data-bs-slide="prev">
-                                        <span class="carousel-control-prev-icon"></span>
-                                    </button>
-                                    <button class="carousel-control-next" type="button" data-bs-target="#post2Carousel" data-bs-slide="next">
-                                        <span class="carousel-control-next-icon"></span>
-                                    </button>
-                                </div>
-
-                                <div class="post-meta">
-                                    <div class="username">@jane_smith</div>
-                                    <div class="post-stats">
-                                        <span class="likes-count"><i class="fas fa-heart"></i> 25</span>
-                                        <span class="comments-count"><i class="fas fa-comment"></i> 10</span>
-                                    </div>
-                                </div>
-
-                                <div class="description" id="desc2">
-                                    This is a sample post about a city. It contains beautiful views and stunning buildings... Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, error recusandae eaque, cumque accusamus temporibus eius atque doloribus fugit quibusdam laborum iusto, incidunt aliquam exercitationem sit voluptatem. Itaque, dolor quasi?
-                                </div>
-                                <span class="see-more" onclick="toggleDescription('desc2', this)">See More</span>
-                            </div>
-                        </div>
-
+                        <?php } ?>
                     </div>
                 </div>
                 <!-- /.container-fluid -->
@@ -194,6 +202,10 @@
 
 
     <script>
+        function openModal(type, postId) {
+            alert("Show " + type + " list for Post ID: " + postId);
+        }
+
         function toggleDescription(id, el) {
             var desc = document.getElementById(id);
             if (desc.style.maxHeight === "60px") {
