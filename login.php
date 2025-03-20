6ece9ade72ab69801e5d4eae3e4636e66a7da350
Akhtar-47 <akhtar47tarakwadiya@gmail.com>
2025-03-20 13:21:40 +0530

    Restriction Added For LoggedIn Admin

--- login.php
+++ login.php
@@ -1,11 +1,15 @@
-<?php session_start(); ?>
+<?php session_start(); 
+    if(isset($_SESSION['admin_id'])){
+        header('location: index.php');
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Admin Login</title>
-    
+
     <link rel="shortcut icon" href="img/logo-removebg-preview.png" type="image/x-icon">
 
     <!-- Custom styles -->
