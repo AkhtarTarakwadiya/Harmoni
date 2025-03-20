88f64002d646925ad492c3226b30698da85ce0ad
Akhtar-47 <akhtar47tarakwadiya@gmail.com>
2025-03-20 09:23:44 +0530

    Fetched User Profile Photo In Followers-Following List

--- api/users_following_api.php
+++ api/users_following_api.php
@@ -7,8 +7,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['method'], $_POST['user_id']) && $_POST['method'] === "fetch_following") {
         $user_id = intval($_POST['user_id']);
 
-        // Fetch Following List
-        $fetchFollowing = "SELECT f.following_id, u.user_name 
+        // Base URL for profile pictures
+        $base_url = "http://192.168.4.220/Harmoni";
+
+        // Fetch Following List with Profile Photo
+        $fetchFollowing = "SELECT f.following_id, u.user_name, u.user_profile_photo 
                            FROM follow_master f 
                            JOIN user_master u ON f.following_id = u.user_id 
                            WHERE f.follower_id = $user_id";
@@ -17,9 +20,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if ($followingResult && mysqli_num_rows($followingResult) > 0) {
             while ($row = mysqli_fetch_assoc($followingResult)) {
+                $profile_photo = !empty($row['user_profile_photo']) ? $base_url . $row['user_profile_photo'] : null;
+
                 $following[] = [
                     "user_id" => (int)$row['following_id'],
-                    "username" => htmlspecialchars($row['user_name'])
+                    "username" => htmlspecialchars($row['user_name']),
+                    "profile_pic" => $profile_photo
                 ];
             }
         }
